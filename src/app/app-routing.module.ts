81800c9f62a859ff42ada7b4803119e75fade58c
chiendh2cm1 <chiendh2cm1@gmail.com>
2022-05-11 14:50:15 +0700

    them navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {AuthGuard} from "./helper/auth.guard";
 import {SignUpComponent} from "./sign-up/sign-up.component";
-import {RecoverPasswordComponent} from "./recover-password/recover-password.component";
+// import {RecoverPasswordComponent} from "./recover-password/recover-password.component";
 
 const routes: Routes = [
   {
@@ -24,10 +24,10 @@ const routes: Routes = [
     path: 'register',
     component: SignUpComponent
   },
-  {
-    path: 'recoverpassword',
-    component: RecoverPasswordComponent
-  }
+  // {
+  //   path: 'recoverpassword',
+  //   component: RecoverPasswordComponent
+  // }
 ];
 
 @NgModule({
